47615cf130f2fcc32fc497783e29fa204b1836b8
James Oswald <jamesoswald111@gmail.com>
2021-04-08 06:36:00 -0400

    multibrot implmented

---
+++ frag.glsl
@@ -0,0 +1,60 @@
+#version 330 core
+
+precision highp float;
+
+out vec4 FragColor;
+uniform vec2 WindowSize;
+uniform float time;
+in vec4 gl_FragCoord;
+
+int maxIterations = 100;
+vec3 c1 = vec3(0, 7, 100);
+vec3 c2 = vec3(2, 107, 203);
+vec3 c3 = vec3(237, 255, 255);
+vec3 c4 = vec3(255, 170, 0);
+
+vec3 palette(float t){
+  float x = 1.0 / 3.0;
+  if (t < x) return mix(c1, c2, t/x);
+  else if (t < 2.0 * x) return mix(c2, c3, (t - x)/x);
+  else if (t < 3.0 * x) return mix(c3, c4, (t - 2.0*x)/x);
+  return c4;
+}
+
+vec2 squareImaginary(vec2 number){
+	return vec2(
+		pow(number.x,2)-pow(number.y,2),
+		2*number.x*number.y
+	);
+}
+
+float hypot(vec2 z) {
+  float x = abs(z.x);
+  float y = abs(z.y);
+  float t = min(x, y);
+  x = max(x, y);
+  t = t / x;
+  //return x * sqrt(1.0 + t * t);
+  return (z.x == 0.0 && z.y == 0.0) ? 0.0 : x * sqrt(1.0 + t * t);
+}
+
+vec2 cpow(vec2 z, float x){
+	float r = hypot(z);
+	float theta = atan(z.y, z.x) * x;
+	return vec2(cos(theta), sin(theta)) * pow(r, x);
+}
+
+vec3 iterateMandelbrot(vec2 coord){
+	vec2 z = vec2(0, 0);
+	for(int i = 0; i < maxIterations; i++){
+		z = cpow(z, 10 * sin(time) + 10) + coord;
+		if(length(z) > 2) 
+            return palette(float(i)/float(maxIterations));
+	}
+	return vec3(0, 0, 0);
+}
+
+void main() {
+    vec2 pos = mat2(3.5/WindowSize.x, 0, 0, 2/WindowSize.y) * gl_FragCoord.xy + vec2(-2.5, -1);
+    FragColor = vec4(iterateMandelbrot(pos), 1.0);
+}
--- saver.c
+++ saver.c
@@ -1,17 +1,135 @@
 
 #include<stdio.h>
-#include<GL/glut.h>
+#include<stdlib.h>
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+#include<time.h>
+#include<math.h>
 
-void draw(){
-    glutSwapBuffers();
+#define uint unsigned int
+
+uint loadShader(char* filename, uint shaderType){
+    FILE* file;
+    file = fopen(filename, "r");
+    fseek(file, 0, SEEK_END);
+    long fsize = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char* glShaderCont = malloc(fsize + 1);
+    fread(glShaderCont, 1, fsize, file);
+    glShaderCont[fsize] = 0;
+    fclose(file);
+
+    uint shader = glCreateShader(shaderType);
+    glShaderSource(shader, 1, (char const* const*)&glShaderCont, NULL);
+    glCompileShader(shader);
+    free(glShaderCont);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        printf("%s failed to compile with error:\n%s", filename, infoLog);
+        exit(1);
+    }
+    return shader;
+}
+
+uint loadProgram(uint vertexShader, uint fragmentShader){
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success){
+        printf("failed to link program with error:\n%s", infoLog);
+        exit(1);
+    }
+    return shaderProgram;
+}
+
+void onResize(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
 
 int main(int argc, char** argv){
-    glutInit(&argc, argv);
-    glutInitWindowSize(600, 600);
-    glutCreateWindow("Opengl");
-    glutFullScreen();
-    //glutDisplayFunc(draw);
-    //system("pause");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glutFullScreen();
+    GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+    const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "MandelBrot", monitor, NULL);
+    if (window == NULL){
+        printf("glfw Init failed");
+        glfwTerminate();
+        exit(1);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, onResize);
+
+    GLenum err = glewInit();
+    if (err != GLEW_OK){
+        printf("GLEW DEAD");
+        exit(1);
+    }
+
+    uint VAO, VBO, shaderProgram;
+    uint vertexShader = loadShader("vert.glsl", GL_VERTEX_SHADER);
+    uint fragmentShader = loadShader("frag.glsl", GL_FRAGMENT_SHADER);
+    shaderProgram = loadProgram(vertexShader, fragmentShader);
+
+    float vertices[] = {
+        // first triangle
+        1,  1,  // top right
+        1, -1,  // bottom right
+        -1, 1, // top left 
+        // second triangle
+        1, -1, // bottom right
+        -1, -1, // bottom left
+        -1,  1, // top left
+    };  
+
+    glGenBuffers(1, &VBO);  
+    glGenVertexArrays(1, &VAO);  
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0); 
+
+    int uWinSize = glGetUniformLocation(shaderProgram, "WindowSize");
+    int uTime = glGetUniformLocation(shaderProgram, "time");
+    glUseProgram(shaderProgram);
+    
+    glBindVertexArray(VAO);
+    clock_t begin = clock();
+    while(!glfwWindowShouldClose(window)){
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        int width, height;
+        glfwGetWindowSize(window, &width, &height);
+        glUniform2f(uWinSize, width, height);
+        glUniform1f(uTime, 10*(clock() - begin)/(float)CLOCKS_PER_SEC);
+
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader); 
+    glDeleteProgram(shaderProgram);
+    glfwTerminate();
+
     return 0;
 }
